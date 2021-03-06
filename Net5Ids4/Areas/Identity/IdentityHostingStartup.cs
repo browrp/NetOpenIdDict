789bb0f6105b1fd53f48b071dea6dc43d3369aaa
Robert Brown <browrp@ucmail.uc.edu>
2021-03-06 16:51:27 -0500

    Scaffolded Identity UI following instructions at https://retifrav.github.io/blog/2020/10/17/dotnet-core-identity-postgresql/

---
+++ Net5Ids4/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Net5Ids4.Data;
+
+[assembly: HostingStartup(typeof(Net5Ids4.Areas.Identity.IdentityHostingStartup))]
+namespace Net5Ids4.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
